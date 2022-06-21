f5c73beb3c2daa4ddcf56373034d9596ce53893a
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-21 11:15:54 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -17,7 +17,7 @@ async def addconnection(client,message):
             cmd, group_id = message.text.split(" ", 1)
         except:
             await message.reply_text(
-                "<b>𝙴𝙽𝚃𝙴𝚁 𝙸𝙽 𝙲𝙾𝚁𝚁𝙴𝙲𝚃 𝙵𝙾𝚁𝙼𝙰𝚃</b>\n"
+                "<b>ᴇɴᴛᴇʀ ɪɴ ᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛ</b>\n"
                 "**/connect 𝙶𝚁𝙾𝚄𝙿 𝙸𝙳\n**"
                 "**Get your Group id by adding this bot to your group and use  <code>/id</code>**",
                 quote=True
@@ -53,7 +53,7 @@ async def addconnection(client,message):
             addcon = await add_connection(str(group_id), str(userid))
             if addcon:
                 await message.reply_text(
-                    f"𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙲𝙾𝙽𝙽𝙴𝙲𝚃 𝚃𝙾 **{title}**\n𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝙼𝙰𝙽𝙰𝙶𝙴 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙵𝚁𝙾𝙼 𝙷𝙴𝚁𝙴../",
+                    f"sᴜᴄᴄᴇssғᴜʟʟʏ ᴄᴏɴɴᴇᴄᴛ ᴛᴏ **{title}**\nɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ғʀᴏᴍ ʜᴇʀᴇ 𝚈𝙾𝚄 𝙲𝙰𝙽 𝙼𝙰𝙽𝙰𝙶𝙴 𝚈𝙾𝚄𝚁 ɢʀᴏᴜᴘ ғʀᴏᴍ ʜᴇʀᴇ",
                     quote=True,
                     parse_mode="md"
                 )
@@ -122,7 +122,7 @@ async def connections(client,message):
             ttl = await client.get_chat(int(groupid))
             title = ttl.title
             active = await if_active(str(userid), str(groupid))
-            act = " › 𝙰𝙲𝚃𝙸𝚅𝙴" if active else ""
+            act = "• ᴀᴄᴛɪᴠᴇ" if active else ""
             buttons.append(
                 [
                     InlineKeyboardButton(
@@ -134,7 +134,7 @@ async def connections(client,message):
             pass
     if buttons:
         await message.reply_text(
-            "𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳 𝙶𝚁𝙾𝚄𝙿𝚂 :-\n\n",
+            "ᴄᴏɴɴᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘs :-\n\n",
             reply_markup=InlineKeyboardMarkup(buttons),
             quote=True
         )
